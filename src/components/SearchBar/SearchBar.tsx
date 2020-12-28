ce2fa5df04129022806f227690535ac025945a2f
Victor Shan <victorshanvs@gmail.com>
2020-12-27 16:30:11 -0800

    meaningless changes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -16,23 +16,21 @@ export default function SearchBar({ onChange }: SearchBarPropsType) {
         }}>
             <InputGroup className={"mb-3"}>
                 <InputGroup.Prepend>
-                    {/* <InputGroup.Text> */}
-                        <Button variant={"outline-dark"}>
-                            <svg 
-                                xmlns="http://www.w3.org/2000/svg" 
-                                width="16" 
-                                height="16" 
-                                fill="currentColor" 
-                                className="bi bi-search" 
-                                viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                            </svg>
-                        </Button>
-                    {/* </InputGroup.Text> */}
+                    <Button variant={"outline-dark"}>
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            width="16" 
+                            height="16" 
+                            fill="currentColor" 
+                            className="bi bi-search" 
+                            viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </Button>
                 </InputGroup.Prepend>
                 
                 <FormControl
-                    placeholder="Press enter to search"
+                    placeholder="Enter a movie name here and press enter"
                     aria-label="Movie Name"
                     aria-describedby="search movie box"
                     onChange={e => setTerms(e.target.value)}
